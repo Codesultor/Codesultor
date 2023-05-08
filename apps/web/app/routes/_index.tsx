9494b627f4109c1c44f024c719733cffa8450fab
Clayton <miqhtiedev@gmail.com>
2023-05-07 21:55:58 -0400

    remix app

---
+++ apps/web/app/routes/_index.tsx
@@ -0,0 +1,13 @@
+import type { V2_MetaFunction } from "@remix-run/node";
+
+export const meta: V2_MetaFunction = () => {
+  return [{ title: "Codesultor" }];
+};
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Codesultor</h1>
+    </div>
+  );
+}
