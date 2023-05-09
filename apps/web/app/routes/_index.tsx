39995659f488f0369554bf25f75c37eb642e4627
Clayton <miqhtiedev@gmail.com>
2023-05-08 20:18:13 -0400

    tailwind

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -7,7 +7,7 @@ export const meta: V2_MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Codesultor</h1>
+      <h1 className="text-blue-600">Codesultor</h1>
     </div>
   );
 }
