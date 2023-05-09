6b8bd904c8884325114323cc4c535ef938beb583
Ryan Arora <arora.ryan@gmail.com>
2023-05-08 20:23:30 -0400

    fix: remove old default style tag

--- apps/web/app/routes/_index.tsx
+++ apps/web/app/routes/_index.tsx
@@ -6,7 +6,7 @@ export const meta: V2_MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+    <div>
       <h1 className="text-blue-600">Codesultor</h1>
     </div>
   );
