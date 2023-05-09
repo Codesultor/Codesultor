2b93c287ff7806e58c442bef025dc52144a32bb9
Ryan Arora <arora.ryan@gmail.com>
2023-05-09 10:33:58 -0400

    feat(fmt): add eslint-plugin-tailwindcss, prettier-plugin-prisma

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,8 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   pluginSearchDirs: false,
-  plugins: [require.resolve("prettier-plugin-jsdoc")],
+  plugins: [
+    require.resolve("prettier-plugin-jsdoc"),
+    require.resolve("prettier-plugin-prisma"),
+  ],
 };
