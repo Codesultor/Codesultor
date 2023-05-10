dd69797c4937566060ab2cdea798affd19174993
Ryan Arora <arora.ryan@gmail.com>
2023-05-10 13:17:51 -0400

    feat(prisma-fmt): add prisma format

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,8 +1,5 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   pluginSearchDirs: false,
-  plugins: [
-    require.resolve("prettier-plugin-jsdoc"),
-    require.resolve("prettier-plugin-prisma"),
-  ],
+  plugins: [require.resolve("prettier-plugin-jsdoc")],
 };
